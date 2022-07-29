30f4b433a8d57d94a2020a03ffd323cf8d0e845a
Christoph86 <ChrstphBch@web.de>
2022-07-29 12:43:41 +0200

    It.2 getting data from axios

--- src/components/ApartmentsList.js
+++ src/components/ApartmentsList.js
@@ -1,4 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function ApartmentsList(){
+
+    const [apartments, setApartments] = useState(null);
+
+    useEffect( () => {
+        fetchApartments();
+      }, []);
+
+    const fetchApartments = () => {
+        axios
+          .get(process.env.REACT_APP_API_BASE_URL + '/apartments')
+          .then((response) => {
+            setApartments(response.data);
+            console.log("axios response:",response.data);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      }
+
+
+
+
     return <p>This is the ApartmentsList</p>
 }
 
